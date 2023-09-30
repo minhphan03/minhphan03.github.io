9ccd38c0706475ca3f4e136565ee75d79071932c
Minh Phan <66846878+minhphan03@users.noreply.github.com>
2023-09-30 16:35:48 -0700

    update photos and music

---
+++ assets/js/get-text.js
@@ -0,0 +1,22 @@
+// of course, ChatGPT
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Array of div IDs corresponding to text file names
+    const divElements = document.querySelectorAll('div[id]'); // Get all div elements with IDs
+
+    divElements.forEach(div => {
+        const divId = div.id;
+
+        // Load the text from the corresponding text file.
+        fetch(`./assets/texts/${divId}.txt`) // Assumes text file names match div IDs
+            .then(response => response.text())
+            .then(text => {
+                // Set the loaded text as the inner HTML of the div.
+                div.innerHTML = text;
+            })
+            .catch(error => {
+                console.error(`Error loading text for ${divId}:`, error);
+            });
+    });
+
+});
